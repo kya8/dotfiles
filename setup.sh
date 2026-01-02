197e5cbe5bdbdaf923f21230c288258056904a6b
kya <46643054+kya8@users.noreply.github.com>
2026-01-02 18:16:24 +0800

    Re-organize config files; Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,38 @@ esac
 
 echo "OS type: ${os_type}"
 
-dotfiles=${dotfiles:-~/.dotfiles}
+dotfiles=$(git -C "$(dirname -- "$0")" rev-parse --show-toplevel)
+echo "Using dotfiles repo: ${dotfiles}"
+
+my_link() {
+    if [ -e "$2" ]; then
+        echo "$2 already exists. Skipping."
+        return 0
+    else
+        ln -s "$1" "$2"
+    fi
+}
+
+supported_os="linux macos freebsd"
+
+link_dir() {
+    for src in "$1"/*; do
+        pkg="${src##*/}"
+        skip=0
+        for os in ${supported_os}; do
+            pkg_name="${pkg%.${os}}"
+            if [ "$pkg_name" != "$pkg" ] ; then
+                if [ "${pkg%.${os_type}}" = "$pkg" ]; then
+                    echo "Skipping ${pkg}"
+                    skip=1
+                fi
+                break;
+            fi
+        done
+        [ ${skip} = 1 ] && continue
+        my_link "$src" "$2/$pkg_name"
+    done
+}
 
 if [ ! -e "${HOME}/.zsh" ]; then
 ln -s "${dotfiles}/zsh" "${HOME}/.zsh" ||:
@@ -37,20 +68,8 @@ ln -s ".zsh/zshrc" "${HOME}/.zshrc" ||:
 ln -s "${dotfiles}/vimrc" "${HOME}/.vimrc" ||:
 
 config_dir=${XDG_CONFIG_HOME:-${HOME}/.config}
+mkdir -p "$config_dir"
 
-# tmux
-# https://github.com/tmux/tmux/issues/142
-tmux_dir=${config_dir}/tmux
-mkdir -p "${tmux_dir}"
-ln -s "${dotfiles}/tmux.conf" "${tmux_dir}/tmux.conf" ||:
+link_dir "${dotfiles}/xdg/config" "$config_dir"
 
-# git
-git_dir=${config_dir}/git
-mkdir -p "${git_dir}"
-ln -s "${dotfiles}/git/config.${os_type}" "${git_dir}/config" ||:
-
-# mpv
-mpv_dir=${config_dir}/mpv
-mkdir -p "${mpv_dir}"
-ln -s "${dotfiles}/mpv/mpv.conf.${os_type}" "${mpv_dir}/mpv.conf" ||:
-ln -s "${dotfiles}/mpv/input.conf" "${mpv_dir}/input.conf" ||:
+echo "Setup done."
