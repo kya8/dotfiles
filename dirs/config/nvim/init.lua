09ab7d52eed1a6fbe30525161a93d3cbde1b7be2
kya <46643054+kya8@users.noreply.github.com>
2026-01-12 01:04:07 +0800

    nvim: Use custom system clipboard keys

--- dirs/config/nvim/init.lua
+++ dirs/config/nvim/init.lua
@@ -31,7 +31,10 @@ opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
 
-opt.clipboard = 'unnamedplus'
+-- opt.clipboard = 'unnamedplus'
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>p", [["+p]], { desc = "Paste from system clipboard" })
 
 opt.splitbelow = true
 
