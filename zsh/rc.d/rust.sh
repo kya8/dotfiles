e5e8e9245adc54822cbff704aeff090e21ee3361
kya <46643054+kya8@users.noreply.github.com>
2026-07-07 00:20:35 +0800

    zsh: Add rust toolchain completions; Move to rc.d

---
+++ zsh/rc.d/rust.sh
@@ -0,0 +1,15 @@
+# Rustup. Activate only if there isn't another active rust installation.
+if ! cmd_exists cargo && [ -r ${HOME}/.cargo/env ]; then
+  source ${HOME}/.cargo/env
+fi
+
+# Completions
+if ! [ -e "${my_fpath}/_rustup" ]; then
+    echo "Generating completions for rustup..."
+    rustup completions zsh > "${my_fpath}/_rustup"
+fi
+if ! [ -e "${my_fpath}/_cargo" ]; then
+    echo "Generating completions for cargo..."
+    rustup completions zsh cargo > "${my_fpath}/_cargo"
+fi
+
