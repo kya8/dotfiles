fd3202eec93471a3df59b2e90a905073cf922652
kya <46643054+kya8@users.noreply.github.com>
2026-07-16 02:38:23 +0800

    zsh: Add yazi shortcuts

---
+++ zsh/rc.d/yazi.sh
@@ -0,0 +1,13 @@
+if cmd_exists yazi; then
+
+alias y=yazi
+
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
+fi
